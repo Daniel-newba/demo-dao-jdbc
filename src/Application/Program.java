02fb1e0e186d5339e982767a5d4bf9c4ac3e7390
Daniel <daniel.nascimento@sette.ag>
2024-09-18 10:34:20 -0300

    adicionando o DAO

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -11,12 +11,14 @@ public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
-        Seller seller = new Seller(21, "bob","dani@gmail.com", new Date(), 3000.0, obj);
+        //Seller seller = new Seller(21, "bob","dani@gmail.com", new Date(), 3000.0, obj);
 
         System.out.println(obj);
-        System.out.println(seller);
+        //System.out.println(seller);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
 
     }
 }
