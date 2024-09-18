3298d3d0c25ef649096c3f41eaa815fb525999bd
Daniel <daniel.nascimento@sette.ag>
2024-09-18 09:14:15 -0300

    criando classes JDBC

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,5 +1,7 @@
 package Application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,7 +16,7 @@ public class Program {
         System.out.println(obj);
         System.out.println(seller);
 
-
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
     }
 }
