52f7e59da4e7111636595635e5f078bb850d86d1
Daniel <daniel.nascimento@sette.ag>
2024-09-19 10:53:40 -0300

    finalizando

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -91,8 +91,6 @@ public class SellerDaoJDBC implements SellerDao {
             st.setInt(1, id);
             st.executeUpdate();
 
-
-
         } catch (SQLException e){
             throw new DbException(e.getMessage());
         } finally {
