c56f6b7b520284b1b125caa2ce3edcbbb69b9264
Daniel <daniel.nascimento@sette.ag>
2024-09-19 10:10:24 -0300

    implementando delete

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -8,10 +8,12 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         SellerDao sellerDao = DaoFactory.createSellerDao();
         //Department obj2 = new Department(1, "Books");
         //Seller seller = new Seller(21, "bob","dani@gmail.com", new Date(), 3000.0, obj);
@@ -45,5 +47,13 @@ public class Program {
         seller.setName("Marta ARA");
         sellerDao.update(seller);
         System.out.println("update completed");
+
+        System.out.println("=== test 6: SELLER INSERT =====");
+        System.out.print("Enter id for delete: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("delete completed.");
+
+        sc.close();
     }
 }
